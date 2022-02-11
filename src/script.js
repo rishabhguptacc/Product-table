b25ca195ec98dc5dee30aa227ade6f086fae45a5
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-11 17:43:01 +0530

    Till now I've done  the project the made edit button work partially.
    things to do are:
    
    1. Button value to change.( add buttton -> update  button).
    
    2. Delete Button.
    
    etc etc...

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 var proTab = [];
 
+// var gId, gName , gPrice ;
+
 function addProduct() {
+  //  var id , name, price = inputBlockFetch(gId, gName, gPrice);
+
   var id = document.getElementById("idInput").value;
   var name = document.getElementById("nameInput").value;
   var price = document.getElementById("priceInput").value;
@@ -11,30 +15,27 @@ function addProduct() {
   productObject.Name = name;
   productObject.Price = price;
 
-  
-
-  if (ifIdExists(id))
-   alert("Id already exists.");
-  else
-    proTab.push(productObject);
+  if (ifIdExists(id)) alert("Id already exists.");
+  else proTab.push(productObject);
 
   outputTable();
 }
 
+// function inputBlockFetch(id , name  , price){
 
+//   var id = document.getElementById("idInput").value;
+//   var name = document.getElementById("nameInput").value;
+//   var price = document.getElementById("priceInput").value;
+//   return id, name, price;
 
-function ifIdExists(id){
+// }
 
-  for(var i= 0; i<proTab.length; i++)
-    {
-      if ( proTab[i].Id == id )
-        return true;
-    }
-    return false;
-
+function ifIdExists(id) {
+  for (var i = 0; i < proTab.length; i++) {
+    if (proTab[i].Id == id) return true;
+  }
+  return false;
 }
-
-
 
 function outputTable() {
   var returnOut =
@@ -47,19 +48,72 @@ function outputTable() {
 
   for (var i = 0; i < proTab.length; i++) {
     returnOut +=
-      "<tr> <td> " +
+      "<tr> \
+        <td> " +
       proTab[i].Id +
-      "</td><td> " +
+      "</td>\
+        <td> " +
       proTab[i].Name +
-      "</td><td> " +
+      "</td>\
+        <td> " +
       proTab[i].Price +
-      "</td><td> " +
-      '<a href = "#" onclick = editList()> Edit </a>\
-      <a href = "#" onclick = deleteList()> Delete </a>\
-      </td></tr> ';
+      "</td>\
+        <td> " +
+      "<a id='edit' href = '#' \
+        onclick ='editList(\"" +
+      proTab[i].Id +
+      '", "' +
+      proTab[i].Name +
+      '","' +
+      proTab[i].Price +
+      "\")'\
+        >Edit</a>\
+        <a id='edit' href = '#' \
+        onclick ='deleteList(\"" +
+      proTab[i].Id +
+      '", "' +
+      proTab[i].Name +
+      '","' +
+      proTab[i].Price +
+      "\")'\
+        >Delete</a>\
+        </td>\
+      </tr>";
+
+    // onclick ='editList(\""+proTab[i].Id +"\")'\
+
+    // <a id=\"del\" href = "#" \
+    // onclick ="deleteList('+proTab[i].Id +','+ proTab[i].Name+ ','+ proTab[i].Price+')"\
+    // >Delete</a>\
   }
 
   returnOut += "</table>";
 
   document.getElementById("result").innerHTML = returnOut;
+}
+
+function editList(eId, eName, ePrice) {
+  // console.log(eId, eName, ePrice);
+  var Id = eId;
+  var Name = eName;
+  var Price = ePrice;
+
+  document.getElementById("idInput").value = Id;
+  document.getElementById("nameInput").value = Name;
+  document.getElementById("priceInput").value = Price;
+
+  document.getElementById("addProduct").value = "UPDATE PRODUCT";
+
+console.log(document.getElementById("addProduct").value)
+
+  Id = document.getElementById("idInput").value ;
+  Name = document.getElementById("nameInput").value;
+  Price = document.getElementById("priceInput").value;
+
+  // return 
+  // console.log(eId, eName,ePrice);
+}
+
+function deleteList(dId, dName, dPrice) {
+  console.log(dId, dName, dPrice);
 }
